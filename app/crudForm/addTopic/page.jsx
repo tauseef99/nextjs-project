83c8e1d8ab00c86dd35b8ee9f8cf67c397a40a3f
tauseef hussain <tauseefhussain232@gmail.com>
2024-02-06 09:54:08 +0500

    updated nextjs receipe with all functionality including -crud1

--- app/crudForm/addTopic/page.jsx
+++ app/crudForm/addTopic/page.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+
 const page = () => {
-  return <div>add topic list contenlllllllllllllllll</div>;
+  return <div>add topic .............</div>;
 };
+
+export default page;
